bf13a439b9a9abe3f3e2c286d8d46345a8cb4863
John Cris <johncrismanabo9@gmail.com>
2022-10-19 13:25:06 +0800

    approved/disapproved user

--- user/recruitment/approve.php
+++ user/recruitment/approve.php
@@ -1,54 +1,54 @@
 <?php include 'session.php';
 /*include '../assets/includes/dbcon.php'; 
 */
-	$id = $_REQUEST['id'];
-	
+	$id = $_REQUEST['id'];  
 
-		$smtp_email ='ryan15reydiamare@gmail.com';
-		$smtp_password = 
+	$query=mysqli_query($con,"select * from donor where donor_id='$id'")or die(mysqli_error($con));
+	$row=mysqli_fetch_array($query);  
+	$donor_email = ucwords($row['donor_email']);  
+	$lastname = $row['donor_last'];  
 
 
-		$this->load->library('phpmailer_lib');
-			 
-		$mail = $this->phpmailer_lib->load();
-			$mail->SMTDebug = 2;
-			$mail->IsSMTP();  
-			$mail->Host = "smtp-relay.sendinblue.com";
-			$mail->SMTPAuth = true;
-			$mail->Username = 'ryan15reydiamare@gmail.com';
-			$mail->Password = 'ryan15rey';
-			$mail->SMTPSecure = 'SSL';  
-			$mail->Port = 587;  
-			
+	//Import PHPMailer classes into the global namespace
+	//These must be at the top of your script, not inside a function
 
-			$mail->SetFrom('ryan15reydiamare@gmail.com', 'Blood Donation'); 
-			$mail->AddAddress($donor_email);  
-			$mail->Subject = 'Congrats'; 
-			$mail->Body = '<p>Good day Mr/Mrs,</p>
-			<p>
-			Thank You fpor donataing blood !</p>  
-			<br> 
-			<p>Regards,</p>';
-			$mail->AltBody = 'try';
-			$mail->send;
-	echo "Mail has been sent successfully";
-		mysqli_query($con,"update survey set survey_status='Accepted' where survey_id='$id'")or die(mysqli_error($con));
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+	//Load Composer's autoloader
+	require '../../assets/vendor/autoload.php';
+
+	//Create an instance; passing `true` enables exceptions
+	$mail = new PHPMailer; 
+
+
+	$mail->isSMTP();                   
+	$mail->Host = "smtp.gmail.com";                 //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'genevabantiad04@gmail.com';                     //SMTP username
+	$mail->Password   = 'ogjczcuqhsrzpdsi';                      
+	$mail->SMTPSecure = 'ssl';          //Enable implicit TLS encryption
+	$mail->Port       = 465;    
+	$mail->setFrom('genevabantiad04@gmail.com', 'Bag Of Hope');
+	$mail->addAddress($donor_email, 'Address Name');  
 		
-		/*$result = mysqli_query($con, "select donor_email FROM donor WHERE donor_id = '$id' ")or die(mysqli_error($con));
-		$row = mysqli_fetch_array($result);
-		echo $row['donor_id'];*/
+
+	$message = "
+		<p>Good day Mr/Mrs $lastname!</p>
+		<p>Thank You for donataing blood!</p> 
+		<p>Regards,</p>
+	"; 
+	//Content
+	$mail->isHTML(true);                                  //Set email format to HTML
+	$mail->Subject = 'Congrats';
+	$mail->Body    = $message;
+
+	$mail->send();
 	
-		
-			
-
-
-
-							
-
-
+	mysqli_query($con,"update survey set survey_status='Accepted' where survey_id='$id'")or die(mysqli_error($con));
 
 
 	echo "<script type='text/javascript'>alert('Successfully updated status of donor!');</script>";
 	echo "<script>document.location='home.php'</script>";
-	
-?>
+	 
--- user/recruitment/reject.php
+++ user/recruitment/reject.php
@@ -1,8 +1,52 @@
 <?php include 'session.php';
 
-	$id = $_REQUEST['id'];
+
+	$id = $_REQUEST['id'];  
+
+	$query=mysqli_query($con,"select * from donor where donor_id='$id'")or die(mysqli_error($con));
+	$row=mysqli_fetch_array($query);  
+	$donor_email = ucwords($row['donor_email']);  
+	$lastname = $row['donor_last'];  
+
+
+	//Import PHPMailer classes into the global namespace
+	//These must be at the top of your script, not inside a function
+
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\SMTP;
+	use PHPMailer\PHPMailer\Exception;
+
+	//Load Composer's autoloader
+	require '../../assets/vendor/autoload.php';
+
+	//Create an instance; passing `true` enables exceptions
+	$mail = new PHPMailer; 
+
+
+	$mail->isSMTP();                   
+	$mail->Host = "smtp.gmail.com";                 //Set the SMTP server to send through
+	$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+	$mail->Username   = 'genevabantiad04@gmail.com';                     //SMTP username
+	$mail->Password   = 'ogjczcuqhsrzpdsi';                      
+	$mail->SMTPSecure = 'ssl';          //Enable implicit TLS encryption
+	$mail->Port       = 465;    
+	$mail->setFrom('genevabantiad04@gmail.com', 'Bag Of Hope');
+	$mail->addAddress($donor_email, 'Address Name');  
+		
+
+	$message = "
+		<p>Good day Mr/Mrs $lastname!</p>
+		<p>Sorry to inform you that you were not selected based on your survey responses.!</p> 
+		<p>Regards,</p>
+	"; 
+	//Content
+	$mail->isHTML(true);                                  //Set email format to HTML
+	$mail->Subject = 'Rejected';
+	$mail->Body    = $message;
+
+	$mail->send(); 
 	
-			mysqli_query($con,"update survey set survey_status='Rejected' where survey_id='$id'")or die(mysqli_error($con));
+	mysqli_query($con,"update survey set survey_status='Rejected' where survey_id='$id'")or die(mysqli_error($con));
 
 	echo "<script type='text/javascript'>alert('Successfully updated status of donor!');</script>";
 	echo "<script>document.location='home.php'</script>";  
