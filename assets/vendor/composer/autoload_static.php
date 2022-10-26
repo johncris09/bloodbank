53cb611a01d1f8b1e88f61d3dcb91797d68ccc2f
John Cris <johncrismanabo9@gmail.com>
2022-10-26 09:27:22 +0800

    autload

--- assets/vendor/composer/autoload_static.php
+++ assets/vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit307c5bf6b3462b0d990c1fa653aa5c1c
 {
     public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Twilio\\' => 7,
+        ),
         'P' => 
         array (
             'PHPMailer\\PHPMailer\\' => 20,
@@ -14,6 +18,10 @@ class ComposerStaticInit307c5bf6b3462b0d990c1fa653aa5c1c
     );
 
     public static $prefixDirsPsr4 = array (
+        'Twilio\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twilio/sdk/src/Twilio',
+        ),
         'PHPMailer\\PHPMailer\\' => 
         array (
             0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
